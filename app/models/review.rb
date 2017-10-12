cd578816fb9cf643181025a64e213819b34e8769
Parmvir <parmvirsthind@gmail.com>
2017-10-12 22:41:46 +0000

    manually added a review to review database

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,8 @@
 class Review < ActiveRecord::Base
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :rating, presence:true
 end
+
+# numericality: { only_integer: true, greater_than: 0, less_than: 6}
