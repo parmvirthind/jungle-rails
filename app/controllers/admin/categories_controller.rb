0367b66a12bde75513b9fd944ccf60b14e4e5e98
Parmvir <parmvirsthind@gmail.com>
2017-10-12 21:39:22 +0000

    implemented basic http authentication for admin pages

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['NAME'], password: ENV['PASSWORD']
+
   before_filter :authorize
 
   def index
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['NAME'], password: ENV['PASSWORD']
+
   before_filter :authorize
 
   def index
