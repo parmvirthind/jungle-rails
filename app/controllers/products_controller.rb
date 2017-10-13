34bc1a47b1e0ed066af75e9cd3ce5bdcb3919649
Parmvir <parmvirsthind@gmail.com>
2017-10-13 00:35:17 +0000

    Added reviews controller and empty form to product details page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,8 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-    @review = Review.where("product_id = ?", @product.id)
+    @reviews = Review.where("product_id = ?", @product.id)
+    @review = Review.create
   end
 
 end
